e47bd0d194d8f098ceca448c060f7a34b414ad55
Darlan Talisson <119646089+dtalisson@users.noreply.github.com>
2026-04-26 17:03:09 -0300

    Update all.bat

--- all.bat
+++ all.bat
@@ -7,20 +7,9 @@ openfiles >nul 2>&1 || (
 )
 
 echo.
-echo [!] O comando a seguir (Clear-Tpm) pode solicitar reinicializacao em alguns sistemas.
-echo [!] Aguardando 10 segundos para voce ler os logs anteriores...
-timeout /t 10
-
+echo [+] Executando Clear-Tpm e desativando AutoProvisioning...
 powershell -Command "Clear-Tpm"
 powershell -Command "Disable-TpmAutoProvisioning"
-
-echo [+] Localizacao atual do script: "%~dp0"
-if exist "%~dp0ac.sys" (
-    echo [+] Encontrado ac.sys em "%~dp0ac.sys"
-) else (
-    echo [!] ERRO: ac.sys NAO ENCONTRADO em "%~dp0ac.sys"
-    dir "%~dp0"
-)
 
 sc stop ac >nul 2>&1
 sc delete ac >nul 2>&1
@@ -28,5 +17,4 @@ sc create ac type= kernel start= auto binPath= "%~dp0ac.sys"
 sc start ac
 
 echo.
-echo Processo de TPM finalizado. Verifique os logs acima.
-pause
+echo [+] Processo de TPM finalizado com sucesso.
